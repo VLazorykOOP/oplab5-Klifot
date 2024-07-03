63b4bd00d5129f600706670f55fbbd8cd2ba0529
Asmareal <Asmareal@DESKTOP-58FEAKF>
2024-07-03 13:49:04 +0300

    LAb5

---
+++ Form1.Designer.cs
@@ -0,0 +1,44 @@
+﻿namespace KochFractalTriangle
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Koch Fractal Triangle";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace KochFractalTriangle
+{
+    public partial class Form1 : Form
+    {
+        private int depth = 4;  // Порядок фракталу
+        private PointF P1 = new PointF(100, 100);
+        private PointF P2 = new PointF(400, 100);
+        private PointF P3 = new PointF(250, 400);
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.pictureBox1.Paint += new PaintEventHandler(Form1_Paint);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White);
+
+            // Малювання кривих Коха на сторонах трикутника
+            DrawKochCurve(g, P1, P2, depth);
+            DrawKochCurve(g, P2, P3, depth);
+            DrawKochCurve(g, P3, P1, depth);
+        }
+
+        private void DrawKochCurve(Graphics g, PointF p1, PointF p2, int depth)
+        {
+            if (depth == 0)
+            {
+                g.DrawLine(Pens.Black, p1, p2);
+            }
+            else
+            {
+                float deltaX = p2.X - p1.X;
+                float deltaY = p2.Y - p1.Y;
+
+                PointF p3 = new PointF(p1.X + deltaX / 3, p1.Y + deltaY / 3);
+                PointF p4 = new PointF(
+                    (float)(0.5 * (p1.X + p2.X) + Math.Sqrt(3) * (p1.Y - p2.Y) / 6),
+                    (float)(0.5 * (p1.Y + p2.Y) + Math.Sqrt(3) * (p2.X - p1.X) / 6)
+                );
+                PointF p5 = new PointF(p1.X + 2 * deltaX / 3, p1.Y + 2 * deltaY / 3);
+
+                DrawKochCurve(g, p1, p3, depth - 1);
+                DrawKochCurve(g, p3, p4, depth - 1);
+                DrawKochCurve(g, p4, p5, depth - 1);
+                DrawKochCurve(g, p5, p2, depth - 1);
+            }
+        }
+    }
+}
